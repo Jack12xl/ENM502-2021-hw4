bbcba34be1743f03a156afa0355f1dd980552247
Jack12xl <jack12xieling@163.com>
2021-04-29 03:40:58 +0800

    update readme

---
+++ FieldL2Err.m
@@ -0,0 +1,24 @@
+function[err] = FieldL2Err(f1, f2, l, h)
+%% L2 distance of fine grid and coarse grid
+% f1: coarse grid
+% f2: fine grid
+% n: one dim of f1
+
+%% for simplicity, we assume 
+% 
+% * n2 = 2n1 - 1 (no 2d interpolation)
+% * field is square
+% 
+% 
+
+% get the corresponding value on fine grid
+    f_tmp = f2(1:2:2*l-1, 1:2:2*h-1);
+    
+    err_map = (f_tmp - f1).^2;
+    figure();
+    contour(err_map);
+    title_str = sprintf("x grid: %d, y grid: %d", l, h);
+    title(title_str);
+    colorbar;
+    err = norm(f_tmp - f1) / (l * h);
+end
--- main.m
+++ main.m
@@ -1,9 +1,9 @@
 clear all; clc
 %% run base case
-L=10; H=2; alpha=1;D=1;k=1; c0=1;
+L=10; H=2; alpha=1;D=100;k=1; c0=1;
 Nx =100; Ny=50;
 x = fem(Nx, Ny, L, H, alpha, D, k , c0);
 
 %%
-title_str = sprintf('H=%d, L=%d, alpha=%d, D=%d, k=%d', H, L, alpha, D, k);
+title_str = sprintf('H=%d, L=%d, alpha=%d, D=%d, k=%d, c0=%d', H, L, alpha, D, k, c0);
 drawMesh(x, Nx, Ny, L, H, title_str);
